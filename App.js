e2e87bd0480693d608fabd2b175c8a90ac385c7e
shivani0614 <shivanialeti14@gmail.com>
2023-12-31 15:58:51 -0700

    Initial TODO Task, with few Styled CSS

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
+import react from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+        <View style={styles.items}>
+            
+            <Task text={'Task 1'}/>
+            <Task text={'Task 2'}/>
+            
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +22,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+    
+  },
+  tasksWrapper:{
+    padding:80,
+    paddingHorizontal:20,
+  },
+  sectionTitle:{
+    fontSize:24,
+    color:'green',
+    fontWeight:'bold',
+    
+  },
+  items:{
+    marginTop:30,
   },
 });
